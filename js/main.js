c8903d53c23e048f9bda452116313f43437c0c99
Nuckle <coil@itscat.org>
2024-10-04 00:49:43 +0300

    Imrove animation, remove unnecessary JS

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-const cards = document.querySelectorAll('.list__item');
+const cards = document.querySelectorAll(".list__item");
 
 for (let i = 0; i < cards.length; i++) {
-    const card = cards[i];
-    const cardNumber = i + 1;
-    const cardNumberElement = card.querySelector('.number');
+	const card = cards[i];
+	const cardNumber = i + 1;
+	const cardNumberElement = card.querySelector(".number");
 
-    cardNumberElement.innerHTML = cardNumber;
-};
-
-const menu = document.querySelector('.header-list');
-const body = document.querySelector('body');
-const burger = document.querySelector('.header__burger');
-
-document.addEventListener('click', (e) => {
-
-    if (e.target === burger) {
-        menu.classList.toggle('header__list--show');
-    } else if (!(e.target === menu)) {
-        menu.classList.remove('header__list--show');
-    }
-})
-
-const scrollBtn = document.querySelector('.scroll-btn');
-const footer = document.querySelector('footer')
-
-window.onscroll = ( ) =>  {
-    // console.log(document.body.clientHeight);
-    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-        scrollBtn.classList.add('scroll-btn--visible')
-      } else if (document.body.scrollTop >= (document.body.scrollTop + footer.offsetHeight)) {
-        console.log('biba')
-      } 
-      else {
-        scrollBtn.classList.remove('scroll-btn--visible')
-      }
+	cardNumberElement.innerHTML = cardNumber;
 }
 
-scrollBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;    
-})
+const menu = document.querySelector(".header-list");
+const body = document.querySelector("body");
+const burger = document.querySelector(".header__burger");
+
+document.addEventListener("click", (e) => {
+	if (e.target === burger) {
+		menu.classList.toggle("header__list--show");
+	} else if (!(e.target === menu)) {
+		menu.classList.remove("header__list--show");
+	}
+});
+
+const scrollBtn = document.querySelector(".scroll-btn");
+const footer = document.querySelector("footer");
+
+window.onscroll = () => {
+	if (
+		document.body.scrollTop > 70 ||
+		document.documentElement.scrollTop > 70
+	) {
+		scrollBtn.classList.add("scroll-btn--visible");
+	} else {
+		scrollBtn.classList.remove("scroll-btn--visible");
+	}
+};
+
+scrollBtn.addEventListener("click", () =>
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	}),
+);
